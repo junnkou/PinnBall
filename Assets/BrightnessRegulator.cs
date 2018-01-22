e5f54896c537e3469e59fcaac63b6285ddac0031
junho <kanndu9@gmail.com>
2018-01-22 22:01:39 +0900

    PinnBallのアセット
    PinnBallのアセット

---
+++ Assets/BrightnessRegulator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class B : MonoBehaviour {
+    Material myMaterial;
+    private float minEmission = 0.3f;
+    private float magEmission = 2.0f;
+    private int degree = 0;
+    private int speed = 10;
+    Color defultColor = Color.white;
+	void Start () {
+        if (tag == "SmallStarTag") { this.defultColor = Color.white; }
+        else if (tag == "LargeStarTag"){ this.defultColor = Color.yellow;}
+        else if(tag == "SmallCloudTag" || tag == "largeStarTag"){ this.defultColor = Color.blue; }
+        this.myMaterial = GetComponent<Renderer>().material;
+        myMaterial.SetColor("_EmissionColor", this.defultColor*minEmission);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.degree >= 0) { Color emissionColor = this.defultColor * (this.minEmission + Mathf.Sin(this.degree * Mathf.Deg2Rad) * this.magEmission);
+            myMaterial.SetColor("_EmissionColor", emissionColor);
+            this.degree -= this.speed;
+        }
+	}
+    private void OnCollisionEnter(Collision other)
+    {
+        this.degree = 180;
+    }
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+    private GameObject scoreText;
+    private int score = 0;
+    private int smallstarscore = 5;
+    private int largestarscore = 10;
+    private int cloudscore = 15;
+	// Use this for initialization
+	void Start () {
+        this.scoreText = GameObject.Find("Score");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag== "SmallStarsTag")
+        {
+            this.score += smallstarscore;
+
+        }
+        else if (collision.gameObject.tag == "largeStarTag")
+        {
+            this.score += largestarscore;
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag" || collision.gameObject.tag == "LargeCloudTag")
+        {
+            this.score += cloudscore;
+        }
+        scoreText.GetComponent<Text>().text = score.ToString();
+    }
+}
